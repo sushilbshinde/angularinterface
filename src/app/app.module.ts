9c865ca16fe918ad310aa9b23fcf54d5dc3a415f
Sushi Shinde <sushilbshinde@gmail.com>
2019-03-08 09:35:22 +0530

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule
