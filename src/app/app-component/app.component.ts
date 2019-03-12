f6e0001c71876f671545f8c0c69090ad0c841820
Sushi Shinde <sushilbshinde@gmail.com>
2019-03-12 08:29:17 +0530

    cross-component events

--- src/app/app-component/app.component.ts
+++ src/app/app-component/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { without } from 'lodash';
 
 library.add(faTimes);
 
@@ -13,6 +14,10 @@ export class AppComponent implements OnInit{
   title = 'Wisdom Pet Medicine';
   theList:object[];
 
+  deleteApt(theApt:object){
+    this.theList = without(this.theList, theApt);
+  }
+  
   constructor(private http: HttpClient){}
   ngOnInit(): void {
     this.http.get<Object[]>('../assets/data.json').subscribe(data=> {
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,4 +6,9 @@ import { Component, Input } from '@angular/core';
 })
 export class ListComponent {
   @Input() aptList;
+  @Output() deleteEvt = new EventEmitter();
+
+  handleDelete(theApt: object) {
+    this.deleteEvt.emit(theApt);
+  }
 }
