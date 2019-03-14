fbebbc1493547bbcdc78608dca014848196104da
Sushi Shinde <sushilbshinde@gmail.com>
2019-03-15 00:30:26 +0530

    updating records

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -7,8 +7,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class ListComponent {
   @Input() aptList;
   @Output() deleteEvt = new EventEmitter();
+  @Output() updateEvt = new EventEmitter();
 
   handleDelete(theApt: object) {
     this.deleteEvt.emit(theApt);
+  }
+
+  handleUpdate(theApt: object, labelName: string, newValue: string) {
+    this.updateEvt.emit({
+      theApt: theApt,
+      labelName: labelName,
+      newValue: newValue
+    })
   }
 }
