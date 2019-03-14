a18b43576238858c0ffcb4bd2b39ee8cad5c52a8
Sushi Shinde <sushilbshinde@gmail.com>
2019-03-14 08:32:30 +0530

    modifying dropdown list

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
-
+  @Input() orderBy;
+  @Input() orderType;
   @Output() queryEvt = new EventEmitter<string>();
 
   handleQuery(query: string) {
